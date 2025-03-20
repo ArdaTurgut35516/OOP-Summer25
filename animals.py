e4f297a0b08d95d49068789b7e8e1b85cda4fb8f
ArdaTurgut35516 <arda.turgut@akademiata.edu.pl>
2025-03-20 14:09:41 +0100

    animals.py

---
+++ animals.py
@@ -0,0 +1,6 @@
+animals = [
+    {'name': 'Cat', 'group': 'Mammals', 'number_of_legs': 4, 'skills': ['climbing', 'jumping', 'hunting']},
+    {'name': 'eagle', 'group': 'birds', 'number_of_legs': 2, 'skills': ['flying', 'hunting']},
+    {'name': 'Shark', 'group': 'Fish', 'number_of_legs': 0, 'skills': ['swimming', 'hunting']},
+    {'name': 'Frog', 'group': 'Amphibians', 'number_of_legs': 4, 'skills': ['jumping', 'swimming']},
+    {'name': 'Cobra', 'group': 'Reptiles', 'number_of_legs': 0, 'skills': ['slithering', 'venomous bite']}
